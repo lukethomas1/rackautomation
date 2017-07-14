3e1cf16a89b1e46e42c2bfff0b63c125e7380705
Luke Thomas <lukethethomas@gmail.com>
2017-07-14 13:56:44 -0700

    adding autotestfiles

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Used for rackspace node naming and some other naming, will break things if changed
-NODE_PREFIX = "node"
+NODE_PREFIX = "node-"
 
 SAVE_FILE = "twonodes"
 
-IMAGE_NAME = "doubleReader"
+IMAGE_NAME = "authorized"
 
-RACK_KEY = "luke-home"
+RACK_KEY = "luke-laptop"
 
 JAR_FILE = "gvine-java.jar"
 
